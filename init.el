01c2e286811e94ca58c6a9ff1b86e43ee208ff4c
Yiding Jia <yiding.jia@gmail.com>
2017-11-13 23:54:17 -0800

    first

---
+++ init.el
@@ -0,0 +1,25 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(package-initialize)
+
+;; Customizations.
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(make-backup-files nil)
+ '(package-selected-packages (quote (evil)))
+ '(show-trailing-whitespace t)
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+
+(require 'evil)
+(evil-mode 1)
