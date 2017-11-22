acd28d5ed167eacedf312f08da8236bf4dfd681e
Yiding Jia <yiding@fb.com>
2017-11-21 19:39:53 -0800

    Proportional font by default, some other packages, autorevertmode

--- init.el
+++ init.el
@@ -9,8 +9,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(global-auto-revert-mode t)
  '(make-backup-files nil)
- '(package-selected-packages (quote (evil)))
+ '(package-selected-packages (quote (php-mode haskell-mode evil)))
  '(show-trailing-whitespace t)
  '(tool-bar-mode nil))
 (custom-set-faces
@@ -18,8 +19,16 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:inherit nil :stipple nil :background "White" :foreground "Black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "nil" :family "Palatino")))))
 
 
 (require 'evil)
 (evil-mode 1)
+
+;; Because sometimes we need monospace fonts.
+
+(defun use-monospace ()
+  "Switch the current buffer to a monospace font."
+  (face-remap-add-relative 'default '(:family "Monospace")))
+
+(add-hook 'dired-mode-hook 'use-monospace)
