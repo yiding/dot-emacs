69262c005e69f0b7a6870bbf168e2feb62aa8f82
Yiding Jia <yiding@fb.com>
2017-12-07 12:45:37 -0800

    fonts and themes

--- init.el
+++ init.el
@@ -9,9 +9,19 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("da8e6e5b286cbcec4a1a99f273a466de34763eefd0e84a41c71543b16cd2efac" default)))
+ '(default-frame-alist
+    (quote
+     ((width . 120)
+      (height . 40))))
+ '(dired-auto-revert-buffer (quote dired-directory-changed-p))
  '(global-auto-revert-mode t)
+ '(inhibit-startup-screen t)
  '(make-backup-files nil)
- '(package-selected-packages (quote (php-mode haskell-mode evil)))
+ '(package-selected-packages (quote (color-theme-modern php-mode haskell-mode evil)))
+ '(scroll-bar-mode nil)
  '(show-trailing-whitespace t)
  '(tool-bar-mode nil))
 (custom-set-faces
@@ -19,8 +29,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "White" :foreground "Black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "nil" :family "Palatino")))))
+ '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "nil" :family "Linux Libertine O")))))
 
+;; evil.
 
 (require 'evil)
 (evil-mode 1)
@@ -29,6 +40,10 @@
 
 (defun use-monospace ()
   "Switch the current buffer to a monospace font."
-  (face-remap-add-relative 'default '(:family "Monospace")))
+  (face-remap-add-relative 'default '(:family "Monospace" :height 120)))
 
 (add-hook 'dired-mode-hook 'use-monospace)
+
+;; Color theme
+(load-theme 'desert t t)
+(enable-theme 'desert)
