88456499e5bd6c5c49b84ab0e010324340ddc047
Yiding Jia <yiding@fb.com>
2018-02-16 18:48:34 -0800

    Add global hook for variable pitch mode, add helm, move color scheme to GUI only.

--- init.el
+++ init.el
@@ -18,17 +18,24 @@
  '(dired-auto-revert-buffer (quote dired-directory-changed-p))
  '(fill-column 80)
  '(global-auto-revert-mode t)
+ '(helm-always-two-windows t)
  '(inhibit-startup-screen t)
  '(intero-stack-executable "/Users/yiding/.local/bin/stack")
  '(make-backup-files nil)
  '(package-selected-packages
    (quote
-    (racer flycheck-rust tuareg bazel-mode intero toml-mode rust-mode yaml-mode markdown-mode indent-guide org-evil evil-org color-theme-modern php-mode haskell-mode evil)))
+    (helm helm-mode-manager exec-path-from-shell racer flycheck-rust tuareg bazel-mode intero toml-mode rust-mode yaml-mode markdown-mode indent-guide org-evil evil-org color-theme-modern php-mode haskell-mode evil)))
  '(scroll-bar-mode nil)
  '(show-trailing-whitespace t)
  '(tool-bar-mode nil)
  '(uniquify-min-dir-content 2)
  '(vc-handled-backends (quote (RCS CVS SVN SCCS SRC Bzr Git))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
 
 ;; one-off scripts go here.
 (add-to-list 'load-path "~/.emacs.d/scripts")
@@ -39,14 +46,27 @@
 (require 'evil)
 (evil-mode 1)
 
+;; osx specific tweaks
+
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize))
+
 
 ; Consider underscores as part of words.
 (modify-syntax-entry ?_ "w")
+
+; strip trailing whitespace on save.
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
 
 ;; Dired hide details by default
 (add-hook 'dired-mode-hook 'dired-hide-details-mode)
 
 ;; Configure some UI specific things.
+(define-globalized-minor-mode my-global-variable-pitch-mode buffer-face-mode
+  (lambda ()
+    (when (not (memq major-mode '(dired-mode shell-mode)))
+      (variable-pitch-mode 1))))
+
 
 ; Tweak haskell syntax highlighting.
 (defun haskell-mode-tweaks ()
@@ -59,15 +79,13 @@
   (set-face-bold-p 'bold nil)
   (add-hook 'haskell-mode-hook
 	    (lambda ()
-	      (variable-pitch-mode)
 	      (haskell-mode-tweaks)))
-  (add-hook 'rust-mode-hook 'variable-pitch-mode)
-  (add-hook 'python-mode-hook 'variable-pitch-mode)
+  (my-global-variable-pitch-mode 1)
+
+  ; These theme is nice in UI, but not so in terminal.
+  (load-theme 'desert t t)
+  (enable-theme 'desert)
   )
 
 ;; BUCK files are python.
 (add-to-list 'auto-mode-alist '("BUCK\\'" . python-mode))
-
-;; Color theme
-(load-theme 'desert t t)
-(enable-theme 'desert)
